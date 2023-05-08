60c326a0adeedabd0c150eb3417ae9d0d7174789
jad-delgadillo <59192230+jad-delgadillo@users.noreply.github.com>
2023-05-08 17:47:12 -0600

    display all icons a user created on a collection page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,13 +11,25 @@ export default function Header() {
   return (
     <header className="fixed w-screen ">
       <div className="mx-2 mt-2 flex h-[4.5rem] max-w-7xl items-center justify-between rounded-xl border border-cyan-800 p-[0.2rem] shadow-lg shadow-emerald-800 backdrop-blur-lg md:mx-auto">
-        <div className=" flex h-[4.4rem] w-screen items-center justify-between  rounded-xl p-3 px-2 backdrop-blur-lg md:px-20">
+        <div className=" flex h-[4.4rem] w-screen flex-row items-center justify-between  rounded-xl p-3 px-2 backdrop-blur-lg md:px-20">
           <Link
             className="rounded-lg p-1 transition-all hover:bg-neutral-900"
             href={"/"}
           >
             <h3 className="rounded-full border p-2 ">alw</h3>
           </Link>
+
+          {isLoggedIn && (
+            <div className="">
+              <Link
+                className="rounded-lg p-3 font-semibold ring-1 ring-white transition-all hover:bg-gray-950 hover:text-indigo-500 hover:ring-indigo-600"
+                href={"/collection"}
+              >
+                My collection
+              </Link>
+            </div>
+          )}
+
           <ul className="flex items-center justify-center gap-4">
             {isLoggedIn && (
               <>
---
+++ src/pages/collection.tsx
@@ -0,0 +1,47 @@
+import { api } from "@/utils/api";
+import { type Icon } from "@prisma/client";
+import { type NextPage } from "next";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import Image from "next/image";
+import React, { useState } from "react";
+
+const CollectionPage: NextPage = () => {
+  const icons = api.icons.getIcons.useQuery();
+
+  const session = useSession();
+  const isLoggedIn = !!session.data;
+
+  return (
+    <>
+      <Head>
+        <title>My collection icons</title>
+        <meta name="description" content="My collection icons" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className=" container flex min-h-screen flex-col bg-zinc-950 px-5 ">
+        {!isLoggedIn && <></>}
+        {isLoggedIn && <></>}
+        <div className="mt-[8rem] flex flex-col gap-2 rounded-lg bg-zinc-900 p-2">
+          <h1 className="">My collection icons</h1>
+          <p>Fill the form below to start generatig your icons.</p>
+          <ul className="grid grid-cols-1 items-center justify-center gap-4 md:grid-cols-6">
+            {icons.data?.map((icon: Icon) => (
+              <li key={icon.id}>
+                <Image
+                  src={`https://alw-icon-generator.s3.us-east-2.amazonaws.com/${icon.id}`}
+                  alt={icon.prompt ?? "an image of an icon"}
+                  width={100}
+                  height={100}
+                  className="w-full rounded-lg "
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default CollectionPage;
